d974f5c5ea18cad627171d2c7f0be9f9073dc4b8
Kristopher Owen <kristopherowen11@gmail.com>
2025-04-30 15:10:20 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\FakultasController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::resource('/fakultas', FakultasController::class);
