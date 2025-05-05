9e6110c8b0572708e6d27e58142ed806fe67b96d
Kristopher Owen <kristopherowen11@gmail.com>
2025-05-05 15:12:52 +0700

    .

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,34 @@
+<h1>Mahasiswa</h1>
+
+<table>
+    <tr>
+        <th>Nama</th>
+        <th>NPM</th>
+        <th>Jenis Kelamin</th>
+        <th>Tanggal</th>
+        <th>Tempat</th>
+        <th>Asal SMA</th>
+        <th>Prodi</th>
+    </tr>
+    @foreach ($mahasiswa as $item)
+    <tr>
+        <td>{{ $item->nama }}</td>
+        <td>{{ $item->npm}}</td>
+        <td>{{ $item->jenis_kelamin}}</td>
+        <td>{{ $item->tanggal_lahir}}</td>
+        <td>{{ $item->tempat_lahir}}</td>
+        <td>{{ $item->asal_sma}}</td>
+        <td>{{ $item->prodi->nama}}</td>
+    </tr>
+    @endforeach
+</table>
+
+@extends('layout.main')
+@section('content')
+<h1>Mahasiswa</h1>\
+@foreach ( $mahasiswa as $item )
+    {{ $item ->npm}} | {{
+        
+    }}
+
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,4 @@ Route::get('/profil', function () {
 
 Route::resource('/fakultas', FakultasController::class); 
 Route::resource('/prodi', ProdiController::class); // menambahkan route resource prodi
+Route::resource('/mahasiswa', MahasiswaController::class); // menambahkan route resource mahasiswa
