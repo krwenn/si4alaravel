b66e6af2ae4ab94010f5ca5ac42361f619fb77a5
Kristopher Owen <kristopherowen11@gmail.com>
2025-05-07 14:40:58 +0700

    Push

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,7 +1,36 @@
 @extends('layout.main')
 @section('content')
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">List Falkutas</h3>
+              <div class="card-tools">
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-collapse"
+                  title="Collapse"
+                >
+                  <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                  <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-remove"
+                  title="Remove"
+                >
+                  <i class="bi bi-x-lg"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">Start creating your amazing application!</div>
+            <!-- /.card-body -->
 
-<h1>Fakultas</h1>
+            <h1>Fakultas</h1>
 <table>
     <tr>
         <th>Nama</th>
@@ -19,3 +48,8 @@
 @endforeach
 </table>
 @endsection
+
+          <!-- /.card -->
+        </div>
+      </div>
+      <!--end::Row-->
